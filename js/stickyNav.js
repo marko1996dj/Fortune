1d6e44deb0a1f6eaf4ea04398b09066a950912b7
marko1996dj <marko96djuric@gmail.com>
2018-12-08 07:14:51 -0800

    mavBar shadow

--- js/stickyNav.js
+++ js/stickyNav.js
@@ -4,8 +4,9 @@ function stickyNav() {
 
   var navbar = document.getElementById("navBarWrapper");
   var sticky = navbar.offsetTop;
+  var yOffset = window.pageYOffset;
 
-  if (window.pageYOffset >= sticky) {
+  if (yOffset >= sticky) {
     navbar.classList.add("sticky")
   } else  {
     navbar.classList.remove("sticky");
