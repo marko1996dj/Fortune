39506b2a0ad541c6a091dd1c1f3aa59db82ccfcc
marko1996dj <marko96djuric@gmail.com>
2018-12-07 16:19:01 -0800

    google map

---
+++ js/function.js
@@ -0,0 +1,10 @@
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+
+//AIzaSyDHJ0P_hhqvPoqJD3pSRz6nEK9DUnyeSsI
