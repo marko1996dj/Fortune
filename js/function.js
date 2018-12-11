c1f771cfa1efc38da240ae57edb3e38f3f9f1ec4
marko1996dj <marko96djuric@gmail.com>
2018-12-11 11:26:38 -0800

    Slick Slider

--- js/function.js
+++ js/function.js
@@ -8,10 +8,65 @@ var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
 // Gmaps End
 
-// Slick function
-// $(document).ready(function(){
-//   $('.your-class').slick({
-//     setting-name: setting-value
-//   });
-// });
-// Slick function end
+//Slick Start
+$(function() {
+  $(document).ready(function() {
+    $('.clients-slider').slick({
+      mobile: true,
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      responsive: [
+   {
+     breakpoint: 1024,
+     settings: {
+       slidesToShow: 2,
+       slidesToScroll: 2,
+       infinite: true,
+       dots: true
+     }
+   },
+   {
+     breakpoint: 600,
+     settings: {
+       slidesToShow: 2,
+       slidesToScroll: 2
+     }
+   },
+   {
+     breakpoint: 480,
+     settings: {
+       slidesToShow: 2,
+       slidesToScroll: 2
+     }
+   }
+   // You can unslick at a given breakpoint now by adding:
+   // settings: "unslick"
+   // instead of a settings object
+ ]
+    });
+
+    $('.testimonials-slider').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        }
+      ]
+    });
+  });
+});
+//Slick End
